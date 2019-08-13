3845696c7a9986e0ee08a8116593020d5aaf0dd7
Watthanai Chotcheewasunthorn <watthanai.chot@aware.co.th>
2019-08-13 17:13:16 +0700

    add get method

--- router/bot-router.js
+++ router/bot-router.js
@@ -2,6 +2,9 @@ import {Router} from 'express';
 import logger from '../utils/logger';
 
 var router = Router()
+router.get('/', (req, res) => {
+  res.send('GET bot')
+})
 router.post('/', (req, res) => {
   res.send('Bot')
   logger.info('path: /bot/')
--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,8 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
-  format: winston.format.json,
+  format: winston.format.json(),
+  defaultMeta: {service: 'user-service'},
   transports: [
     new winston.transports.File({filename: 'error.log', level: 'error'}),
     new winston.transports.File({filename: 'all.log'})
